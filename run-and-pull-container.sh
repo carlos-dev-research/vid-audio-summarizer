36f5071a87b5654f2e4894784a51a259c2a5261f
Carlos <68810007+carlos-dev-research@users.noreply.github.com>
2024-06-08 01:49:22 -0400

    Delete run-and-pull-container.sh

--- run-and-pull-container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-IMAGE_NAME="carlosdevresearch/tube-summarizer"
-
-docker pull $IMAGE_NAME
-# Command to run docker with gpus enabled, mount the folder Docs and use the interactivily the bash shell
-docker run --gpus all  -p 5000:5000  $IMAGE_NAME
