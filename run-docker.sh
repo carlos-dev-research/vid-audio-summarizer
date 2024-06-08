2d09269335463311396b190049ea9b1b3b014315
Carlos <68810007+carlos-dev-research@users.noreply.github.com>
2024-06-08 02:15:15 -0400

    Update run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -2,4 +2,4 @@
 IMAGE_NAME="tube-summarizer:02"
 
 # Command to run docker with gpus enabled, mount the folder Docs and use the interactivily the bash shell
-docker run --gpus all  -p 5000:5000  $IMAGE_NAME
+docker run --gpus all  -p 5000:5000  -d $IMAGE_NAME
